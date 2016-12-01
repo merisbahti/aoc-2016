71d68af8a2be9ef829d23ec620cbe6faaed4160c
meris <meris.bahti@gmail.com>
2016-12-01 18:07:35 +0100

    Solve problem 1.

--- src/a1/sol.js
+++ src/a1/sol.js
@@ -1,8 +1,57 @@
-/* @flow */
-const a: number = 3
-function addOne(x: number) {
-  return x + 1
+const dist = (input) => {
+  const directions = parseInput(input)
+  const startingState = {
+    previousCoordinates: [[0, 0]],
+    firstVisitedTwice: undefined,
+    direction: 0, // four directions, 0, 1, 2, 3, where 0 is north, 3 is west
+    north: 0,
+    east: 0
+  }
+
+  const reducer = (state, vector) => {
+    const changedDirection = changeDirection(state, vector.d)
+    return calculateSteps(changedDirection, vector.l)
+  }
+
+  const ans = directions.reduce(reducer, startingState)
+
+  return {dist: Math.abs(ans.north) + Math.abs(ans.east), firstVisitedTwice: ans.firstVisitedTwice}
 }
-console.log(addOne(a))
-console.log(addOne('a'))
-export default addOne
+const parseInput = (input) => input.split(', ').map(s => { return {d: s[0], l: parseInt(s.slice(1))} })
+
+const changeDirection = (state, newDirection) => {
+  const jsModWhichIsNotWeird = (n, m) => {
+    return ((n % m) + m) % m
+  }
+  return Object.assign({}, state,
+    {
+      direction:
+      jsModWhichIsNotWeird(state.direction + (newDirection === 'R' ? 1 : (newDirection === 'L' ? -1 : 0)), 4)
+    }
+  )
+}
+
+const calculateSteps = (state, steps) => {
+  return [...Array(steps)].reduce((state, _) => {
+    const northD = (state.direction === 0 ? 1 : (state.direction === 2 ? -1 : 0))
+    const eastD = (state.direction === 1 ? 1 : (state.direction === 3 ? -1 : 0))
+    const newCoord = [state.north + northD, state.east + eastD]
+    // check for all previous coordinates
+    if (state.firstVisitedTwice === undefined &&
+      state.previousCoordinates.find((x) => (x[0] === newCoord[0] && x[1] === newCoord[1]))) {
+      return Object.assign({}, state, {
+        firstVisitedTwice: newCoord,
+        previousCoordinates: [...state.previousCoordinates, newCoord],
+        north: newCoord[0],
+        east: newCoord[1]
+      })
+    }
+    return Object.assign({}, state, {
+      previousCoordinates: [...state.previousCoordinates, newCoord],
+      north: newCoord[0],
+      east: newCoord[1]
+    })
+  }, state)
+}
+
+module.exports = {dist, calculateSteps, changeDirection}
--- test/a1/test.js
+++ test/a1/test.js
@@ -1,9 +1,24 @@
-const a1 = require('../../src/a1/sol.js')
-var assert = require('assert')
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1, 2, 3].indexOf(4))
-    })
-  })
+const readFile = require('fs').readFile
+readFile('./testInput', 'utf8', (err, contents) => {
+  console.log(err)
+  console.log(contents)
 })
+
+const sol = require('../../src/a1/sol.js')
+const { dist } = sol
+const prob = `R4, R1, L2, R1, L1, L1, R1, L5, R1, R5, L2, R3, L3, L4, R4, R4, R3, L5, L1, R5, R3, L4, R1, R5, L1, R3, L2, R3, R1, L4, L1, R1, L1, L5, R1, L2, R2, L3, L5, R1, R5, L1, R188, L3, R2, R52, R5, L3, R79, L1, R5, R186, R2, R1, L3, L5, L2, R2, R4, R5, R5, L5, L4, R5, R3, L4, R4, L4, L4, R5, L4, L3, L1, L4, R1, R2, L5, R3, L4, R3, L3, L5, R1, R1, L3, R2, R1, R2, R2, L4, R5, R1, R3, R2, L2, L2, L1, R2, L1, L3, R5, R1, R4, R5, R2, R2, R4, R4, R1, L3, R4, L2, R2, R1, R3, L5, R5, R2, R5, L1, R2, R4, L1, R5, L3, L3, R1, L4, R2, L2, R1, L1, R4, R3, L2, L3, R3, L2, R1, L4, R5, L1, R5, L2, L1, L5, L2, L5, L2, L4, L2, R3`
+const inputsAnswersMap = {
+  'R2, L3': 5,
+  'R2, R2, R2': 2,
+  'R5, L5, R5, R3': 12
+}
+
+Object.keys(inputsAnswersMap).forEach(k => {
+  const corr = inputsAnswersMap[k]
+  const ans = dist(k)
+  console.log(`For input: ${k}, answer is ${corr} and you got: ${ans.dist}
+which is: ${corr === ans.dist ? 'correct' : 'false'}`)
+})
+const probAnswer = dist(prob)
+console.log(`For the problem, the answer is: ${probAnswer.dist},
+and the first point visited twice is ${probAnswer.firstVisitedTwice}`)
