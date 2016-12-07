88b099411951274f793dc9e4e1bbc1195aa36143
meris <meris.bahti@gmail.com>
2016-12-07 05:54:47 +0100

    solve (sloppily) problem 6

--- src/a6/sol.js
+++ src/a6/sol.js
@@ -1,7 +1,30 @@
 const frequencyAnalysis = (input) => {
   const rows = input.split('\n').map(row => row.split(''))
-  rows.inject((acc, x) => {
+  const rowFreq = rows.reduce((acc, row) => {
+    row.forEach((c, i) => {
+      var newObj = {}
+      newObj[c] = (acc[i][c] || 0) + 1
+      acc[i] = Object.assign(acc[i], newObj)
+    })
+    return acc
+  }, {0: {}, 1:{}, 2:{}, 3:{}, 4:{}, 5:{}, 6:{}, 7:{}})
+
+  const ans = Object.keys(rowFreq).reduce((acc, rowIndex) => {
+    const ansI = Object.keys(rowFreq[rowIndex]).reduce(({max, currMaxChar}, char) => {
+      if (rowFreq[rowIndex][char] < max) {
+        return {max: rowFreq[rowIndex][char], currMaxChar: char}
+      } else {
+        return {max, currMaxChar}
+      }
+    },
+      {
+        max: 100000000000,
+        currMaxChar: undefined
+      })
+    acc[rowIndex] = ansI
+    return acc
   }, {})
-  return 'east0r'
+  console.log(ans)
+  return ans
 }
 module.exports = { frequencyAnalysis }
